c8bfa5b07ef2489f4d8b68f88603f04fa30f1a76
tudaicts <tuda@icts.vn>
2023-11-16 16:38:04 +0700

    trim files. update vite config

--- src/app/modules/auth/AuthPage.tsx
+++ src/app/modules/auth/AuthPage.tsx
@@ -1,19 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import { Registration } from "./components/Registration";
-import { ForgotPassword } from "./components/ForgotPassword";
-import { Login } from "./components/Login";
-import { AuthLayout } from "./AuthLayout";
 import { StepSignUp } from "./StepSignUp";
 
 const AuthPage = () => (
   <Routes>
     <Route path="step-sign-up" element={<StepSignUp />} />
     <Route index element={<StepSignUp />} />
-    <Route element={<AuthLayout />}>
-      <Route path="login" element={<Login />} />
-      <Route path="registration" element={<Registration />} />
-      <Route path="forgot-password" element={<ForgotPassword />} />
-    </Route>
   </Routes>
 );
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/app",
+  base: "/demo.react.metronic/app",
   build: {
     chunkSizeWarningLimit: 3000,
   },
